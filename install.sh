782000a7c3fee8027536133a5a54fbe0e1cd5846
Demir Yerli <36820091+mrquantumoff@users.noreply.github.com>
2022-06-12 15:46:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sh -c 'curl -fsLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 curl -fssL -o ~/.config/nvim/init.vim https://gist.github.com/mrquantumoff/cc4a914c4b97a09fd312d07a33aa3bed/raw/edba212140514d269c8884eee97342216d815d88/.nvimrc
 nvim +PlugInstall
-nvim +CocInstall coc-rust-analyzer +OmniSharpInstall 
+nvim +OmniSharpInstall +CocInstall coc-rust-analyzer 
 export rustanalyzerlocation=$(which rust-analyzer)
 rsanc="{rust-analyzer.server.path: \"$rustanalyzerlocation\"}"
 echo $rsanc
