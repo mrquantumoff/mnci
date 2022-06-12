3dc6e10693f2d6ccb84deb0cd72427aea8a8baa6
Demir Yerli <36820091+mrquantumoff@users.noreply.github.com>
2022-06-12 18:32:12 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pkcon install neovim -y
 mkdir -p ~/.config/nvim
 sh -c 'curl -fsLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-curl -fssL -o ~/.config/nvim/init.vim https://gist.github.com/mrquantumoff/cc4a914c4b97a09fd312d07a33aa3bed/raw/edba212140514d269c8884eee97342216d815d88/.nvimrc
+curl -fssL -o ~/.config/nvim/init.vim https://gist.github.com/mrquantumoff/cc4a914c4b97a09fd312d07a33aa3bed/raw/29344db04bce0620948d65467a56c7c9d4a28605/.nvimrc
 nvim +PlugInstall
 nvim +OmniSharpInstall +CocInstall coc-rust-analyzer 
 export rustanalyzerlocation=$(which rust-analyzer)
