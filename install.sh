f7f4fee4e7895551e0932247757b7509e0928edd
Demir Yerli <36820091+mrquantumoff@users.noreply.github.com>
2022-06-12 15:18:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,10 @@ sh -c 'curl -fsLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug
 curl -fssL -o ~/.config/nvim/init.vim https://gist.github.com/mrquantumoff/cc4a914c4b97a09fd312d07a33aa3bed/raw/ba9466c8a80d30fbd9d36b80ae719df5e94da44f/.nvimrc
 nvim +PlugInstall
 nvim +CocInstall coc-rust-analyzer
+export rustanalyzerlocation=$(where rust-analyzer)
+rsanc="{\"rust-analyzer.server.path\": \"$rustanalyzerlocation\"}"
+echo $rsanc
+echo $rsanc >> ~/.config/nvim/coc-settings.json
+
 ln -s ~/.config/nvim/init.vim ~/.nvimrc
 echo "NeoVim and config installed!"
