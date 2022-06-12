910bf1abc6925598b9ea7abc2203c33f3eec9598
Demir Yerli <36820091+mrquantumoff@users.noreply.github.com>
2022-06-12 14:59:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pkcon install neovim -y
 mkdir -p ~/.config/nvim
 sh -c 'curl -fsLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-curl -fssL -o ~/.config/nvim/init.vim https://gist.github.com/mrquantumoff/cc4a914c4b97a09fd312d07a33aa3bed/raw/7099f823e311044868eca9c7a0f961bf646401a7/.nvimrc
+curl -fssL -o ~/.config/nvim/init.vim https://gist.github.com/mrquantumoff/cc4a914c4b97a09fd312d07a33aa3bed/raw/ba9466c8a80d30fbd9d36b80ae719df5e94da44f/.nvimrc
 nvim +PlugInstall
 ln -s ~/.config/nvim/init.vim ~/.nvimrc
 echo "NeoVim and config installed!"
