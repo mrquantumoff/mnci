aeb612d39d43779b844b224a155b05694a26f63a
Demir Yerli <36820091+mrquantumoff@users.noreply.github.com>
2022-06-12 15:13:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ mkdir -p ~/.config/nvim
 sh -c 'curl -fsLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 curl -fssL -o ~/.config/nvim/init.vim https://gist.github.com/mrquantumoff/cc4a914c4b97a09fd312d07a33aa3bed/raw/ba9466c8a80d30fbd9d36b80ae719df5e94da44f/.nvimrc
-nvim +PlugInstall +CocInstall coc-rust-analyzer
+nvim +PlugInstall
+nvim +CocInstall coc-rust-analyzer
 ln -s ~/.config/nvim/init.vim ~/.nvimrc
 echo "NeoVim and config installed!"
